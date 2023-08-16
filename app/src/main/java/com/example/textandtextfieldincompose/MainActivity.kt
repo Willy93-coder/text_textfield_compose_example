a9ca04a77b4d56b64b3bde4e0c3aca1ca57d029a
Willy <guillecg13@gmail.com>
2023-08-16 11:39:52 +0200

    Added: basic textfield component

--- app/src/main/java/com/example/textandtextfieldincompose/MainActivity.kt
+++ app/src/main/java/com/example/textandtextfieldincompose/MainActivity.kt
@@ -5,10 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
@@ -29,12 +35,22 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MyText()
+                    MyTextField()
                 }
             }
         }
     }
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyTextField() {
+    var myText by remember {
+        mutableStateOf("")
+    }
+    TextField(value = myText, onValueChange = { myText = it })
+}
+
 
 @Composable
 fun MyText() {
